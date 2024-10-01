288859f880e57e4cbd68814f5e4e75bdee7b0ae1
okclover <93367728+okclover@users.noreply.github.com>
2024-10-01 16:21:07 +0900

    수정

--- helloworld.js
+++ helloworld.js
@@ -2,3 +2,15 @@ console.log('hello world')
 var str = 'javaScript';
 var arr = str.split("");
 console.log('newList:', arr)
+
+const obj = {
+    name:'lsi', 
+    age: 22, 
+    hobby: ['운동', '수면', '먹방'],
+    weight: 67,
+}
+console.log()
+
+
+
+
